47aca624c240873fe8d5881a03fe0ee26135f6f0
DM107 <stak7085@gmail.com>
2024-10-27 15:36:16 +0530

    how to addstyle  in react.js

--- Component/JavascriptXML.jsx
+++ Component/JavascriptXML.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
+import '../src/App.css'
 
 const Greeting = ({name}) =>{
 
     const isMoring = new Date().getHours < 12;
+
+    const divStyle = {
+      backgroundColor: "red",
+      padding: "30px",
+      border: "2px solid black",
+      color: "blue",
+      marginTop: "5px"
+    }
 
     return(
         <>
             <h1>Hello: {name}</h1>
             <p>Good: {isMoring ? "Moring" : "Evening"}</p>
             <p>The cureent tine is: {new Date().toString()}</p>
+
+          <div style={{backgroundColor: "red", padding: "10px", border: "1px solid black", color: "blue"}}>Style</div>
+          <div style={divStyle}>style 1</div>
+          <div className='divstyle'>style 2</div>
+
         </>
     )
 }
