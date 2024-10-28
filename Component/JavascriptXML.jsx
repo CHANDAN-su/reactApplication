3d0efc77f591e5f097ce1e78c32eb09573de6479
DM107 <stak7085@gmail.com>
2024-10-28 19:29:51 +0530

    Using Keys in Lists

--- Component/JavascriptXML.jsx
+++ Component/JavascriptXML.jsx
@@ -1,11 +1,13 @@
-import React, {useCallback, useState} from 'react'
+import React, { useCallback, useState } from 'react'
 import '../src/App.css'
 
-const Greeting = ({name}) =>{
+const Greeting = ({ name }) => {
 
   const [isLoggedIN, setIsLoggedIN] = useState(false);
   const [hasMessage, setHasMessage] = useState(false);
   const [status, setStatus] = useState("guest");
+
+  
 
   const userShow = () => {
     switch (status) {
@@ -20,83 +22,95 @@ const Greeting = ({name}) =>{
     }
   }
 
-    const handleLogInClick = () => {
-      setIsLoggedIN(true);
-    }
+  const handleLogInClick = () => {
+    setIsLoggedIN(true);
+  }
 
-    const handleLogOutClick = () => {
-      setIsLoggedIN(false);
-    }
+  const handleLogOutClick = () => {
+    setIsLoggedIN(false);
+  }
 
-    let button;
+  let button;
 
-    if(isLoggedIN){
-      button = <button className='divstyle' onClick={handleLogOutClick}>Logout</button>
-    }else{
-      button = <button className='divstyle' onClick={handleLogInClick}>Log in</button>
-    }
+  if (isLoggedIN) {
+    button = <button className='divstyle' onClick={handleLogOutClick}>Logout</button>
+  } else {
+    button = <button className='divstyle' onClick={handleLogInClick}>Log in</button>
+  }
 
-    const isMoring = new Date().getHours < 12;
+  const isMoring = new Date().getHours < 12;
 
-    const divStyle = {
-      backgroundColor: "red",
-      padding: "30px",
-      border: "2px solid black",
-      color: "blue",
-      marginTop: "5px"
-    }
+  const divStyle = {
+    backgroundColor: "red",
+    padding: "30px",
+    border: "2px solid black",
+    color: "blue",
+    marginTop: "5px"
+  }
 
-    const fruit = ["Apple", "Banana", "Orange", "Kiwi"];
+  const fruit = ["Apple", "Banana", "Orange", "Kiwi"];
 
-    return(
-        <>
-            <h1>Hello: {name}</h1>
-            <p>Good: {isMoring ? "Moring" : "Evening"}</p>
-            <p>The cureent tine is: {new Date().toString()}</p>
+  const todo = [
+    { id: 1, text: 'Learn React' },
+    { id: 2, text: 'Build a Project' },
+    { id: 3, text: 'Deploy the App' }
+  ]
 
-          <div style={{backgroundColor: "red", padding: "10px", border: "1px solid black", color: "blue"}}>Style</div>
-          <div style={divStyle}>style 1</div>
-          <div className='divstyle'>style 2</div>
+  return (
+    <>
+      <h1>Hello: {name}</h1>
+      <p>Good: {isMoring ? "Moring" : "Evening"}</p>
+      <p>The cureent tine is: {new Date().toString()}</p>
 
-          <ul>
-            {fruit.map((fr) => (
-              <li key={fr}>{fr}</li>
-            ))}
-          </ul>
+      <div style={{ backgroundColor: "red", padding: "10px", border: "1px solid black", color: "blue" }}>Style</div>
+      <div style={divStyle}>style 1</div>
+      <div className='divstyle'>style 2</div>
 
-            {/* <h1>{isLoggedIN? "Welcome Back" : "Please Log in"}</h1>
+      <ul>
+        {fruit.map((fr) => (
+          <li key={fr}>{fr}</li>
+        ))}
+      </ul>
+
+      {/* <h1>{isLoggedIN? "Welcome Back" : "Please Log in"}</h1>
             {button} */}
 
-            <h1>{isLoggedIN ? "Welcome back" : "Please log in"}</h1>
-            <button onClick={() => setIsLoggedIN(!isLoggedIN)}>{isLoggedIN ? "Logout" : "Login"}</button>
+      <h1>{isLoggedIN ? "Welcome back" : "Please log in"}</h1>
+      <button onClick={() => setIsLoggedIN(!isLoggedIN)}>{isLoggedIN ? "Logout" : "Login"}</button>
 
-            <h1>Inbox</h1>
-            {hasMessage && <p>You have new message!</p>}
-            <button onClick={() => setHasMessage(!hasMessage)}>
-              {hasMessage ? "Hide message" : "Show nessage"}
-            </button>
+      <h1>Inbox</h1>
+      {hasMessage && <p>You have new message!</p>}
+      <button onClick={() => setHasMessage(!hasMessage)}>
+        {hasMessage ? "Hide message" : "Show nessage"}
+      </button>
 
-            {userShow()}
-            <button onClick={() => setStatus("guest")}>Guest</button>
-            <button onClick={() => setStatus("user")}>User</button>
-            <button onClick={() => setStatus("admin")}>Admin</button>
+      {userShow()}
+      <button onClick={() => setStatus("guest")}>Guest</button>
+      <button onClick={() => setStatus("user")}>User</button>
+      <button onClick={() => setStatus("admin")}>Admin</button>
 
 
-            <ul>
+      <ul>
 
-              {fruit.map((val, index) => (
-                <li key={index}>{val}</li>
-              ))}
-              
-            </ul>
+        {fruit.map((val, index) => (
+          <li key={index}>{val}</li>
+        ))}
 
-        </>
-    )
+      </ul>
+
+      <ul>
+        {todo.map((val) => (
+          <ol key={val.id}>{val.text}</ol>
+        ))}
+      </ul>
+
+    </>
+  )
 }
 
 function JavascriptXML() {
   return (
-    < Greeting name={"chanan thakur"}/>
+    < Greeting name={"chanan thakur"} />
   )
 }
 
