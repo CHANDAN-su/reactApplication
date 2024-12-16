cff37b1e593724da4a01e4a5138183cb4b721f02
Tony stak <stak7085@gmail.com>
2024-12-16 16:54:19 +0530

    Advanced Example: Passing State to "No Match" Page
    You might want to pass some information (such as the attempted URL) to the "No Match" component

--- Component/RecatRouterDom/NotFound.jsx
+++ Component/RecatRouterDom/NotFound.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useLocation } from "react-router-dom";
 
 function NotFound () {
+
+  const location = useLocation();
+
   return (
-    <div>404 Page NotFound </div>
+    <>
+      <div>404 Page NotFound </div>
+      <p>The page <code>{location.pathname}</code> doesn't exit.</p>
+    </>
   )
 }
 
