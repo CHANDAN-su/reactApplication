13956a9e2c7c0acf66ad9b76a6d58ccd5d621a75
Tony stak <stak7085@gmail.com>
2024-12-18 18:49:21 +0530

    Handling Missing or Default Search Parameters
    Sometimes, not all search parameters may be present in the URL. In such cases, you can handle missing parameters by providing default values.

--- Component/RecatRouterDom/DynamicRoutes/DynamicRouteWithQueryParameter.jsx
+++ Component/RecatRouterDom/DynamicRoutes/DynamicRouteWithQueryParameter.jsx
@@ -11,6 +11,9 @@ const SearcgRoute = () => {
     const category = queryParams.get("category");
     const page = queryParams.get("pages");
 
+    const query12 = queryParams.get("quuery12") || "No Search Query";
+    const page123 = queryParams.get("page123") || 1;
+
     return (
         <>
             <h3>Search Results for: {searchQuery}</h3>
@@ -21,6 +24,12 @@ const SearcgRoute = () => {
                 <p>Category: <strong>{category}</strong></p>
                 <p>Page: <strong>{page}</strong></p>
                 {/* You can add logic to fetch and display products based on these params */}
+            </div>
+
+            <div>
+                <h3>Search Results</h3>
+                <p>Search Query: <strong>{query12}</strong></p>
+                <p>Page: <strong>{page123}</strong></p>
             </div>
         </>
     )
@@ -42,6 +51,12 @@ function DynamicRouteWithQueryParameter() {
                 </li>
                 <li>
                     <Link to="search?query=Javascript&category=books&pages=2">JAvascriptBooks2</Link>
+                </li>
+                <li>
+                    <Link to="search?quuery12=React">Query12</Link>
+                </li>
+                <li>
+                    <Link to="search?page123=2">Page123</Link>
                 </li>
             </nav>
 
