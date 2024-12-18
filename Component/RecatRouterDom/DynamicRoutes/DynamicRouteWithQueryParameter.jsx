577af69a7ceadd56f7cc1d16989e95d3957ad534
Tony stak <stak7085@gmail.com>
2024-12-18 18:20:55 +0530

    Handling Multiple Search Parameters
    If you have multiple search parameters, you can retrieve each one similarly by using queryParams.get('paramName').

--- Component/RecatRouterDom/DynamicRoutes/DynamicRouteWithQueryParameter.jsx
+++ Component/RecatRouterDom/DynamicRoutes/DynamicRouteWithQueryParameter.jsx
@@ -2,17 +2,34 @@ import React from 'react'
 import { Routes, Route, Link, useLocation } from "react-router-dom";
 
 const SearcgRoute = () => {
-    const location  = useLocation();
-    const queryParams = new URLSearchParams(location.search);
-    const searchQuery  = queryParams.get('search');
 
-    return <h3>Search Results for: {searchQuery}</h3>
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.search);
+
+    const searchQuery = queryParams.get('search');
+    const query = queryParams.get("query");
+    const category = queryParams.get("category");
+    const page = queryParams.get("pages");
+
+    return (
+        <>
+            <h3>Search Results for: {searchQuery}</h3>
+
+            <div>
+                <h3>Product Search</h3>
+                <p>Searching for: <strong>{query}</strong></p>
+                <p>Category: <strong>{category}</strong></p>
+                <p>Page: <strong>{page}</strong></p>
+                {/* You can add logic to fetch and display products based on these params */}
+            </div>
+        </>
+    )
 }
 
 function DynamicRouteWithQueryParameter() {
-  return (
-    <>
-        
+    return (
+        <>
+
             <nav>
                 <li>
                     <Link to="search?search=React">Search React</Link>
@@ -20,14 +37,20 @@ function DynamicRouteWithQueryParameter() {
                 <li>
                     <Link to="search?search=JavaScript">Search JavaScript</Link>
                 </li>
+                <li>
+                    <Link to="search?query=React&category=electornics&pages=1">Rectelectronics1</Link>
+                </li>
+                <li>
+                    <Link to="search?query=Javascript&category=books&pages=2">JAvascriptBooks2</Link>
+                </li>
             </nav>
 
             <Routes>
-                <Route path="/" element={<h1>Home Page</h1>}/>
-                <Route path="/search" element={<SearcgRoute/>}/>
+                <Route path="/" element={<h1>Home Page</h1>} />
+                <Route path="/search" element={<SearcgRoute />} />
             </Routes>
-    </>
-  )
+        </>
+    )
 }
 
 export default DynamicRouteWithQueryParameter
