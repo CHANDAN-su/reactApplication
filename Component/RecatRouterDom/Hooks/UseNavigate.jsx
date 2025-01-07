1650ce2cd692fc9dc6b6d21ae8649a19ddfc0d1d
Tony stak <stak7085@gmail.com>
2025-01-07 18:54:17 +0530

    Navigate with State
    You can also pass state along with the navigation. This state can be accessed on the target route using the useLocation hook.

--- Component/RecatRouterDom/Hooks/UseNavigate.jsx
+++ Component/RecatRouterDom/Hooks/UseNavigate.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 
 const Home = () => {
 
@@ -9,11 +9,18 @@ const Home = () => {
         negative("/about");
     }
 
+    const getProfile = () => {
+        
+        negative("/profile", {state: {from: "home"}});
+
+    }
+
     return (
         <>
             <div>
                 <h1>Home page</h1>
-                <button onClick={getAbut}>Go to about</button>
+                <button onClick={getAbut}>Go to About</button>
+                <button onClick={getProfile}>Go to Profile</button>
             </div>
         </>
     )
@@ -23,7 +30,7 @@ const About = () => {
     const negative = useNavigate();
 
     const getHome = () => {
-        negative("/")
+        negative("/");
     }
     
     return (
@@ -36,12 +43,24 @@ const About = () => {
     )
 }
 
+const Profile = () => {
+    const location = useLocation();
+    console.log(location.state);
+    return (
+        <>
+            <h1>Profile Page</h1>
+            <p>This is Profile</p>
+        </>
+    )
+}
+
 function UseNavigate() {
   return (
     <>
         <Routes>
             <Route path='/' element={<Home/>} />
             <Route path='/about' element={<About/>} />
+            <Route path="/profile" element={<Profile/>} />
         </Routes>
     </>
   )
