c267bdf11b13d9460d9076a4629e1098a1b2ea24
Tony stak <stak7085@gmail.com>
2025-01-17 19:01:22 +0530

    Using useState with Strings

--- Component/Hooks/UseState.jsx
+++ Component/Hooks/UseState.jsx
@@ -16,11 +16,23 @@ const Counter = () => {
     )
 }
 
+const Greeting = () => {
+    const [name, setName] = useState("");
+
+    return (
+        <>
+            <input type="text" value={name} placeholder='Enter your name' onChange={(e) => setName(e.target.value)} />
+            <p>Hello, {name || "Gust!"}</p>
+        </>
+    )
+}
+
 
 function UseState() {
   return (
     <>  
         <Counter/>
+        <Greeting/>
     </>
   )
 }
