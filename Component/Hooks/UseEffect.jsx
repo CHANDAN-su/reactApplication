600e096cd35f6d419604c0fc0b35ee89b4c06e86
Tony stak <stak7085@gmail.com>
2025-01-19 15:11:46 +0530

    Effect with Cleanup (Example: Event Listener)
    ### **Dependencies in `useEffect`**
    
    - **Empty Dependency Array (`[]`):**
        - Effect runs only once after the component mounts.
        - Used for setup logic (e.g., API calls, event listeners).
    - **Specific Dependencies (`[value1, value2]`):**
        - Effect runs only when one of the dependencies changes.
    - **No Dependency Array:**
        - Effect runs after every render (and re-render).
    
    ### **Summary**
    
    - `useEffect` is used for managing side effects.
    - It can run:
        - After every render (no dependencies).
        - Only once (empty dependency array).
        - When specific dependencies change.
    - Always clean up subscriptions, intervals, or event listeners to avoid memory leaks.

--- Component/Hooks/UseEffect.jsx
+++ Component/Hooks/UseEffect.jsx
@@ -103,6 +103,36 @@ const SearchComponent = () => {
     )
 }
 
+const MouseTracker = () => {
+    const [position, setPosition] = useState({x: 0, y: 0});
+
+    useEffect(() => {
+
+            const handleMouseMove = (e) => {
+                setPosition({
+                    x: e.clientX,
+                    y: e.clientY
+                })
+            }
+
+            window.addEventListener("mousemove", handleMouseMove);
+
+        return () => {
+            window.removeEventListener("mousemove", handleMouseMove)
+        }
+
+    })
+
+
+    return (
+        <>
+            <h1>
+      Mouse Position: X: {position.x}, Y: {position.y}
+    </h1>
+        </>
+    )
+}
+
 
 function UseEffectDsta() {
 
@@ -137,6 +167,7 @@ function UseEffectDsta() {
         </div>
         <Timer/>
         <SearchComponent/>
+        <MouseTracker/>
     </>
   )
 }
