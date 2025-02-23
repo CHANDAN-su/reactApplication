fa8baffe0e91d57da4baadc5c935877c528db567
chandan thakur <chandan7073251686@gmail.com>
2025-02-23 11:50:06 +0530

    paramas hooks

---
+++ Component/RecatRouterDom/Hooks/AllHooks/UseParams.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider, useParams } from "react-router-dom";
+
+
+const Home = () => {
+    return  (
+        <>
+
+        </>
+    )
+}
+
+const UserProfile = () => {
+    return (
+        <>
+
+        </>
+    )
+}
+
+
+function UseParams() {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default UseParams
