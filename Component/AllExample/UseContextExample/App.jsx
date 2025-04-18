e63fe4833812e032c14c26c2a0ccdff08afeebee
93sahcflas77 <139437921+93sahcflas77@users.noreply.github.com>
2025-04-18 10:36:19 +0000

    Auth Context (Login Status):

--- Component/AllExample/UseContextExample/App.jsx
+++ Component/AllExample/UseContextExample/App.jsx
@@ -1,14 +1,38 @@
+// example 1
+// import React from "react";
+// import { ThemeProvider } from "./ThemeContext/ThemeContext";
+// import ThemedComponent from "./ThemeContext/ThemedComponent";
+
+// function MainApp() {
+
+//     return (
+//             <ThemeProvider>
+//                 <ThemedComponent />
+//             </ThemeProvider>
+//     )
+// }
+
+// export default MainApp;
+
+
+// Example 2
+
 import React from "react";
-import { ThemeProvider } from "./ThemeContext/ThemeContext";
-import ThemedComponent from "./ThemeContext/ThemedComponent";
+import { AuthProivder } from "./AuthContext/AuthContext";
+import LoginControl from "./AuthContext/LoginControl";
+import Profile from "./AuthContext/Profile";
 
-function MainApp() {
-
+function MainApp(){
     return (
-            <ThemeProvider>
-                <ThemedComponent />
-            </ThemeProvider>
+        <AuthProivder>
+                <div style={{textAlign: "center", padding: "40px"}}>
+                    <h2>Auth Context Demo</h2>
+                    <Profile/>
+                    <LoginControl/>
+                </div>
+        </AuthProivder>
     )
 }
+
 
 export default MainApp;
